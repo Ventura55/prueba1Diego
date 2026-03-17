8ba066aa809dd7f8fb34e1825feb551b930ec7b8
Ventura55 <diegoventurduq@gmail.com>
2026-03-17 19:45:29 +0100

    Primer commit

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,6 +5,6 @@ public class Principal {
 	// Cambio 3
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("Pruebas git. se añade Cambio1");
 	}
-
 }
